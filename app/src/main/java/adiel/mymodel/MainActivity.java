00a4a8e675648b9af892665d89e8b61958036e4a
adiel <adielbot@outlook.com>
2017-03-14 17:12:12 +0200

    basic crud with content provider, be aware to all Harcoded strings

--- app/src/main/java/adiel/mymodel/MainActivity.java
+++ app/src/main/java/adiel/mymodel/MainActivity.java
@@ -1,13 +1,62 @@
 package adiel.mymodel;
 
+import android.content.ContentResolver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import adiel.mymodel.data.DatabaseHelper;
+import adiel.mymodel.data.MyDbHelper;
 
 public class MainActivity extends AppCompatActivity {
+    String[] itemname ={
+            "Get theatre tickets",
+            "Order pizza for tonight",
+            "Buy groceries",
+            "Running session at 19.30",
+            "Call Uncle Sam"
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void initDb(View view) {
+        DatabaseHelper helper = new DatabaseHelper(this);
+        helper.getReadableDatabase();
+    }
+
+    public void createCategory(View view) {
+        MyDbHelper myDbHelper = new MyDbHelper();
+        myDbHelper.createCategory(getContentResolver());
+    }
+
+    public void readData(View view) {
+        MyDbHelper myDbHelper = new MyDbHelper();
+        myDbHelper.readData(getContentResolver());
+    }
+
+    public void createTodo(View view) {
+        MyDbHelper myDbHelper = new MyDbHelper();
+        myDbHelper.createTodo(getContentResolver());
+    }
+
+    public void updateData(View view) {
+        MyDbHelper myDbHelper = new MyDbHelper();
+        myDbHelper.updateTodo(getContentResolver());
+    }
+
+    public void deleteTodo(View view) {
+        MyDbHelper myDbHelper = new MyDbHelper();
+        myDbHelper.deleteTodo(getContentResolver());
+    }
+
+
+    public void deleteAllTodo(View view) {
+        MyDbHelper myDbHelper = new MyDbHelper();
+        myDbHelper.deleteAllTodo(getContentResolver());
     }
 }
---
+++ app/src/main/java/adiel/mymodel/data/MyDbHelper.java
@@ -0,0 +1,78 @@
+package adiel.mymodel.data;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+
+import static adiel.mymodel.data.TodosContract.*;
+
+/**
+ * Created by recntrek7 on 14/03/17.
+ */
+
+public class MyDbHelper {
+
+    public void updateTodo(ContentResolver contentResolver) {
+        int id = 2;
+        String[] args = {String.valueOf(id)};
+        ContentValues values = new ContentValues();
+        values.put(TodosContract.TodosEntry.COLUMN_TEXT, "Call Mr Clark Kent");
+        int numRows = contentResolver.update(TodosContract.TodosEntry.CONTENT_URI, values,
+                TodosEntry._ID + "=?",  args);
+        Log.d("Update Rows ", String.valueOf(numRows));
+
+    }
+    public void deleteTodo(ContentResolver contentResolver) {
+        int id = 2;
+        String[] args = {String.valueOf(id)};
+        int numRows = contentResolver.delete(TodosEntry.CONTENT_URI, TodosEntry._ID + " =?", args);
+        Log.d("Delete Rows", String.valueOf(numRows));
+    }
+
+    public void deleteAllTodo(ContentResolver contentResolver) {
+        int numRows = contentResolver.delete(TodosEntry.CONTENT_URI, null, null);
+        Log.d("Delete Rows", String.valueOf(numRows));
+    }
+
+    public void readData(ContentResolver contentResolver) {
+
+        String[] projection = {TodosEntry.COLUMN_TEXT,
+                TodosEntry.COLUMN_CREATED,
+                TodosEntry.COLUMN_EXPIRED,
+                TodosEntry.COLUMN_DONE,
+                TodosContract.CategoriesEntry.COLUMN_DESCRIPTION};
+        String selection = TodosEntry.COLUMN_CATEGORY + " = ?";
+        String[] selectionArgs = {"1"};
+        Cursor c = contentResolver.query(TodosEntry.CONTENT_URI, projection, null, null, null );
+        int i = c.getCount();
+        Log.d("Record Count", String.valueOf(i));
+        String rowContent = "";
+        while (c.moveToNext()) {
+            for (i=0; i<=4; i++) {
+                rowContent += c.getString(i) + " - ";
+            }
+            Log.i("Row " + String.valueOf(c.getPosition()), rowContent);
+            rowContent = "";
+        }
+        c.close();
+    }
+    public void createTodo(ContentResolver contentResolver){
+
+        ContentValues values = new ContentValues();
+        values.put(TodosEntry.COLUMN_TEXT, "Call Mr Pink");
+        values.put(TodosEntry.COLUMN_CATEGORY, 1);
+        values.put(TodosEntry.COLUMN_CREATED, "2016-01-02");
+        values.put(TodosEntry.COLUMN_DONE, 0);
+        Uri uri = contentResolver.insert(TodosEntry.CONTENT_URI, values);
+    }
+
+    public void createCategory(ContentResolver contentResolver){
+        ContentValues values = new ContentValues();
+        values.put(TodosContract.CategoriesEntry.COLUMN_DESCRIPTION, "Work");
+        Uri uri = contentResolver.insert(TodosContract.CategoriesEntry.CONTENT_URI,
+                values);
+        Log.d("MainActivity", "Inserted note " + uri);
+    }
+}
